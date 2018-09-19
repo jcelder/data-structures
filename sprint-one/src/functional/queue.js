d227e970be77d8fadc7fe763818ceb9d5a361ebd
Joshua Elder <jcelder88@gmail.com>
2018-09-19 15:28:35 -0700

    Complete final tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,20 +4,29 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var currentSize = 0;
+  var currentTail = 0;
   var currentHead = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[currentSize] = value;
+    storage[currentTail] = value;
     currentSize++;
+    currentTail++;
   };
 
   someInstance.dequeue = function() {
     if (currentSize === 0) {
       return someInstance.size();
+    } else {
+      var dequeuedItem = storage[currentHead];
+      currentSize--;
+      currentHead++;
+      if (currentHead > currentTail) {
+        currentHead = currentTail;
+      }
+      return dequeuedItem;
     }
-    currentSize--;
   };
 
   someInstance.size = function() {
