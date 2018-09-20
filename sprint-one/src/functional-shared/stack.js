65b7f9a81f1d9a8809b7a69d4c3304eabf04f748
Joshua Elder <jcelder88@gmail.com>
2018-09-19 18:22:14 -0700

    Complete all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = {};
+  stackInstance.currentSize = 0;
+  stackInstance.storage = {};
+
+  _.extend(stackInstance, stackMethods);
+
+  return stackInstance;
 };
 
-var stackMethods = {};
+stackMethods = {};
+stackMethods.push = function(string) {
+  this.storage[this.currentSize] = string;
+  this.currentSize++;
+};
+
+stackMethods.pop = function() {
+  if (this.currentSize === 0) {
+    return this.currentSize;
+  } else {
+    var poppedString = this.storage[this.currentSize - 1];
+    delete this.storage[this.currentSize];
+    this.currentSize--;
+    return poppedString;
+  }
+};
+
+stackMethods.size = function() {
+  return this.currentSize;
+};
+
 
 
