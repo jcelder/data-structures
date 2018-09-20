26033b5c01c23c13982656ba7e19bda9e1b47871
Joshua Elder <jcelder88@gmail.com>
2018-09-19 18:27:48 -0700

    Complete test 1

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = {};
+  queueInstance.storage = {};
+  queueInstance.currentSize = 0;
+  queueInstance.currentHead = 1;
+
+  _.extend(queueInstance, queueMethods);
+  return queueInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function() {};
+queueMethods.dequeue = function() {};
+queueMethods.size = function() {
+  return this.currentSize;
+};
 
