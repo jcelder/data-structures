55dc6cab3d1aa7bfbe5f3068b0d397d5f6378500
jcelder <jcelder88@gmail.com>
2018-09-20 11:24:58 -0700

    completes linked list and passes all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    } else if (list.head === null) {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var removedHead = list.head;
+      list.head = list.head.next;
+      return removedHead.value;
+    }
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    debugger;
+    if (node) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next !== null) {  
+        return list.contains(target, node.next);  
+      }
+    } else {
+      return list.contains(target, list.head);
+    }
+    return false;
   };
 
   return list;
