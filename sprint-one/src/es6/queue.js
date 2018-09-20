87b143b5ac825e3ee1a158577409454485686cfc
Joshua Elder <jcelder88@gmail.com>
2018-09-19 23:02:44 -0700

    Complete all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,35 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.currentSize = 0;
+    this.currentHead = 1;
   }
 
+  enqueue(value) {
+    if (this.currentHead > this.currentSize) {
+      this.storage[this.currentHead + this.currentSize] = value;
+      this.currentSize++;
+    } else {
+      this.currentSize++;
+      this.storage[this.currentSize] = value;
+    }
+  }
+
+  dequeue() {
+    if (this.currentSize === 0) {
+      this.currentHead = 1;
+      return this.currentSize;
+    } else {
+      var dequeuedValue = this.storage[this.currentHead];
+      delete this.storage[this.currentHead];
+      this.currentHead++;
+      this.currentSize--;
+      return dequeuedValue;
+    }
+  }
+
+  size() {
+    return this.currentSize;
+  }
 }
