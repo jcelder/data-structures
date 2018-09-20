80491f07049955f38278c2bb5bcfdb5afba07fa9
Joshua Elder <jcelder88@gmail.com>
2018-09-19 22:33:25 -0700

    Complete all tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = Object.create(stackMethods);
+  stackInstance.storage = {};
+  stackInstance.currentSize = 0;
+
+  return stackInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.currentSize++;
+    this.storage[this.currentSize] = value;
+  },
+  pop: function() {
+    if (this.currentSize === 0) {
+      return this.currentSize;
+    }
+    var poppedValue = this.storage[this.currentSize];
+    delete this.storage[this.currentSize];
+    this.currentSize--;
+    return poppedValue;
+  },
+  size: function() {
+    return this.currentSize;
+  }
+};
 
 
